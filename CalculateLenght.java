6074f3ae9e7bf549e63f8faf9ec2731f5c41edf8
meggie123 <meghanabura@gmail.com>
2022-05-13 16:56:27 +0530

    Calculating Lenght

---
+++ CalculateLenght.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.line_comparison;
+import java.util.Scanner;
+public class CalculateLenght {
+    public static void main(String args[])
+    {
+        int x1,x2,y1,y2;
+
+        double length;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter x1 point");
+
+        x1=sc.nextInt();
+
+        System.out.println("enter y1 point");
+
+        y1=sc.nextInt();
+
+        System.out.println("enter x2point");
+
+        x2=sc.nextInt();
+
+        System.out.println("enter y2 point");
+
+        y2=sc.nextInt();
+
+        length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+
+        System.out.println("Length of Lines is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
+
+
+    }
+
+}
+
